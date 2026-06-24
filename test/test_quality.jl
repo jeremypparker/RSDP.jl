b28405a9a60b08b1e3769e0bf6296dbe878c8476
Jeremy Parker <jparker002@dundee.ac.uk>
2026-06-24 12:04:12 +0100

    only support 1.12

--- test/test_quality.jl
+++ test/test_quality.jl
@@ -3,5 +3,8 @@ using JET
 
 @testset "quality checks" begin
     Aqua.test_all(RSDP; ambiguities=false)
-    JET.test_package(RSDP; target_modules=(RSDP,))
+    if VERSION >= v"1.12.0" && VERSION < v"1.13.0-"
+        using JET
+        JET.test_package(RSDP; target_modules=(RSDP,))
+    end
 end
